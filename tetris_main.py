d508db43f5e7f8f8e2c313dca21c4cd6f67df2f3
safin-dr <safin.dr@phystech.edu>
2020-11-21 02:02:48 +0300

    Подкорректировал работу программы, избавился от ошибок, оформил документацию чуть лучше, улучшил coding slyle (PEP-8)

--- tetris_main.py
+++ tetris_main.py
@@ -1,64 +1,80 @@
-import pygame
-
-from pygame.draw import *
 from random import *
+
+import pygame
+from pygame import *
 
 import tetris_settings as sett
 
+
+# coding UTF-8
+
+
 def calc_x(x):
-    '''
-    Принимает координату x кубика в клетках "стакана". Возвращает экранную координату x левого верхнего угла кубика.
-    '''
-    return sett.glass_x + (x - 1)*sett.cube_edge
+    """Принимает координату "x" кубика в клетках "стакана".
+
+    Возвращает экранную координату "x" левого верхнего угла кубика.
+
+    """
+    return sett.glass_x + (x - 1) * sett.cube_edge
 
 
 def calc_y(y):
-    '''
-    Принимает координату y кубика в клетках "стакана". Возвращает экранную координату y левого верхнего угла кубика.
-    '''
-    return sett.glass_y + (y - 1)*sett.cube_edge
+    """Принимает координату "y" кубика в клетках "стакана".
+
+    Возвращает экранную координату "y" левого верхнего угла кубика.
+
+    """
+    return sett.glass_y + (y - 1) * sett.cube_edge
 
 
 class Cube:
+
     def __init__(self, surface, x, y, color):
         self.surface = surface
-        self.x = x # координата x, выраженная в клетках "стакана"
+        self.x = x  # координата x, выраженная в клетках "стакана"
         self.y = y  # координата y, выраженная в клетках "стакана"
         self.color = color
 
     def draw(self):
-        '''
-        Рисует кубик по координатам в клетках "стакана"
-        '''
+        """Рисует кубик по координатам в клетках "стакана".
+
+        """
         if 0 < self.x < 11 and 0 < self.y < 21:
-            rect(self.surface, self.color, (calc_x(self.x), calc_y(self.y), sett.cube_edge, sett.cube_edge))
-            rect(self.surface, sett.BLACK, (calc_x(self.x), calc_y(self.y), sett.cube_edge, sett.cube_edge), 1)
+            pygame.draw.rect(self.surface, self.color,
+                             (calc_x(self.x), calc_y(self.y), sett.cube_edge, sett.cube_edge))
+            pygame.draw.rect(self.surface, sett.BLACK,
+                             (calc_x(self.x), calc_y(self.y), sett.cube_edge, sett.cube_edge),
+                             1)
 
     def touch_check(self):
-        '''
-        Проверяет свободность соседних с данным кубиком клеток.
-        Возвращает список логических значений, выражающих свободность клеток снизу, слева, сверху и справа от кубика
-        '''
-        return [ glass_list[self.x][self.y + 1],
-                 glass_list[self.x - 1][self.y],
-                 glass_list[self.x][self.y - 1],
-                 glass_list[self.x + 1][self.y]
-                 ]
+        """Проверяет свободность соседних с данным кубиком клеток.
+
+        Возвращает список логических значений, выражающих свободность клеток
+        снизу, слева, сверху и справа от кубика.
+
+        """
+        return [glass_list[self.x][self.y + 1],
+                glass_list[self.x - 1][self.y],
+                glass_list[self.x][self.y - 1],
+                glass_list[self.x + 1][self.y]
+                ]
 
 
 class Figure:
+
     def __init__(self, surface, x, y, color, type):
         self.surface = surface
-        self.x = x # координата x опорного кубика, выраженная в клетках "стакана"
+        self.x = x  # координата x опорного кубика, выраженная в клетках "стакана"
         self.y = y  # координата y опорного кубика, выраженная в клетках "стакана"
         self.color = color
-        self.type = type # тип
-        self.orient = 0 # ориентация фигуры
+        self.type = type
+        self.orient = 0  # ориентация фигуры
 
     def make(self):
         cube_list = []
         for i in sett.figure_dict[self.type][self.orient]:
-            cube_list.append( Cube(self.surface, self.x + i[0], self.y + i[1], self.color) )
+            cube_list.append(Cube(self.surface, self.x + i[0],
+                                  self.y + i[1], self.color))
         return cube_list
 
     def draw(self):
@@ -83,7 +99,7 @@ pygame.init()
 screen = pygame.display.set_mode((sett.width, sett.height))
 
 # Присваивание логических значений клеткам "стакана"
-glass_list = [ [True for j in range(22)] for i in range(12) ]
+glass_list = [[True for j in range(22)] for i in range(12)]
 for i in [0, 11]:
     for j in range(22):
         glass_list[i][j] = False
@@ -93,8 +109,8 @@ for i in range(12):
 # Список ключей фигур
 key_list = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
 
-curr_fig = Figure(screen, 5, 0, choice(sett.colors), choice(key_list)) # Первая фигура
-next_fig = Figure(screen, 5, 0, choice(sett.colors), choice(key_list)) # Фигура, следующая за первой
+curr_fig = Figure(screen, 5, 0, choice(sett.colors), choice(key_list))  # Первая фигура
+next_fig = Figure(screen, 5, 0, choice(sett.colors), choice(key_list))  # Фигура, следующая за первой
 
 # Список неподвижных кубиков, отображаемых на экране
 dead_cubes = []
@@ -117,9 +133,10 @@ while not finished:
             elif event.key == pygame.K_DOWN:
                 curr_fig.turn(-1)
 
-    screen.fill(sett.WHITE) # Фон
+    screen.fill(sett.WHITE)  # Фон
     # Границы стакана
-    rect(screen, sett.BLACK, (sett.glass_x, sett.glass_y, sett.cube_edge * 10, sett.cube_edge * 20), 1)
+    pygame.draw.rect(screen, sett.BLACK, (sett.glass_x, sett.glass_y,
+                                          sett.cube_edge * 10, sett.cube_edge * 20), 1)
 
     curr_fig.draw()
 
@@ -130,6 +147,7 @@ while not finished:
             can_be_moved = False
             break
 
+    # noinspection PyUnboundLocalVariable
     if can_be_moved:
         curr_fig.vert_move()
     else:
@@ -137,11 +155,12 @@ while not finished:
             glass_list[j.x][j.y] = False
             dead_cubes += curr_fig.make()
             curr_fig = next_fig
-            next_fig = Figure(screen, 5, 0, choice(sett.colors), choice(key_list))
+            next_fig = Figure(screen, 5, 0,
+                              choice(sett.colors), choice(key_list))
 
     for i in dead_cubes:
         i.draw()
 
     pygame.display.update()
 
-pygame.quit
+pygame.quit()
