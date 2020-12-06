eae9427245a98bd6cbe575f897165b79bc850432
BerezkinNick <berezkin.nikolay.02@mail.ru>
2020-12-07 02:47:42 +0300

    Реализовано окно со статистикой в меню

--- stat_menu.py
+++ stat_menu.py
@@ -20,4 +20,26 @@ def printer(surface, string, pt, cor):
 
 
 def stat(surface):
-    ...
+    
+    """Отображает окно статистики
+
+    """
+    image = pygame.image.load('1.png')
+    
+    finished = False
+    while not finished:
+        surface.fill(sett.WHITE)
+        back_butt = Buttons.Button()
+        surface.blit(image, (30, 150))
+        printer(surface, 'Статистика', 40, (300, 50))
+
+        back_butt.create_button(surface, sett.WHITE, 375, 650, 150, 60, 3, "Назад", sett.BLACK)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == MOUSEBUTTONDOWN:
+                if back_butt.pressed(pygame.mouse.get_pos()):
+                    finished = True
+        pygame.display.flip()
+
