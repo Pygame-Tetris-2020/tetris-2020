c3bb7c1be9b61ea3f80697b2aee0819975d6fca1
Ivan-Ivashkin <ivashkinir@yandex.ru>
2020-12-06 21:28:08 +0300

    Редактирован модуль box

--- box.py
+++ box.py
@@ -1,21 +1,11 @@
 import pygame
-from pygame import *
-
-import Buttons
 
 import sett
-
 from main_menu import *
-from pause import *
-from box import *
-from figure import *
 from cube import *
-from music import *
-from settings import *
-from game_over import *
+
 
 class Box:
-
     def __init__(self, surface, color, x_left_up, y_left_up, width, height):
         self.surface = surface
         self.color = color  # цвет границы поля
@@ -33,16 +23,35 @@ class Box:
                 self.cells[i][j] = False
 
     def draw(self):
+        """Отвечает за отрисовку поля.
+
+        """
         pygame.draw.rect(self.surface, self.color, (self.x_left_up + 1, self.y_left_up + 1,
                                               sett.cube_edge * self.width, sett.cube_edge * self.height), 1)
 
     def block_cell(self, x, y):
+        """Блокирует клетку.
+
+        Принимает координаты блокируемой клетки.
+
+        """
         self.cells[x][y] = False
 
     def free_cell(self, x, y):
+        """Освобождает клетку.
+
+        Принимает координаты освобождаемой клетки.
+
+        """
         self.cells[x][y] = True
 
     def is_line_free (self, x):
+        """Проверяет свободность линии.
+
+        Принимает вертикальную координату анализируемой линии.
+        Возвращает логическое значение.
+
+        """
         line = False
         for i in [x]:
             for j in range(self.width + 2):
@@ -50,6 +59,12 @@ class Box:
         return line
 
     def destroy_line (self, num, dead_cubes):
+        """Уничтожает заполненную линию.
+
+        Принимает вертикальную координату уничтожаемой линии и список неподвижных кубиков.
+        Возвращает список неподвижных кубиков и флаг уничтожения линии.
+
+        """
         has_been_destroyed = False
         if not self.is_line_free(num):
             has_been_destroyed = True
